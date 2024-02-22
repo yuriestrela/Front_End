6d08245509f45f30fce23ea543ae3da0a8c6d99b
Yuri Estrella <141427497+yuriestrela@users.noreply.github.com>
2024-02-22 15:21:58 -0300

    Gerador de Currículos
    Criação do projeto "Gerador de Currículo".

---
+++ Gerador de Curriculos/script.js
@@ -0,0 +1,37 @@
+function salvar_informacoes(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Salva as informações em algum lugar (por exemplo, localStorage)
+    var nome = document.getElementById('name').value;
+    var idade = document.getElementById('idade').value;
+    var email = document.getElementById('email').value;
+    var telefone = document.getElementById('telefone').value;
+    var endereco = document.getElementById('endereco').value;
+
+    localStorage.setItem('name', nome);
+    localStorage.setItem('idade', idade);
+    localStorage.setItem('email', email);
+    localStorage.setItem('telefone', telefone);
+    localStorage.setItem('endereco', endereco);
+
+    // Redireciona para a próxima página
+    window.location.href = "pagina2.html";
+}
+
+function salvar_informacoes2(event) {
+    event.preventDefault();
+
+    var objetivo = document.getElementById('objetivo').value;
+    var experiencia = document.getElementById('experiencia').value;
+    var qualificacao_profissional = document.getElementById('qualificacao_profissional').value;
+    
+    localStorage.setItem('objetivo', objetivo);
+    localStorage.setItem('experiencia', experiencia);
+    localStorage.setItem('qualificacao_profissional', qualificacao_profissional);
+
+    window.location.href = "pagina3.html"
+}
+
+function voltar(){
+    window.history.back();
+}
