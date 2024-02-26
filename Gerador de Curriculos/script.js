12eb0691a1bf932bfcc0b2b53728a438c45c787e
yuriestrela <yuriestrela2016@gmail.com>
2024-02-26 17:47:40 -0300

    Second Commit
    Criação da página 4 e 5;
    Mudança na página de qualificação e experiência profissional;
    Retirei todos os "required" para facilitar os testes;
    Tentativa de criar barras de níveis nas qualificações profissionais;

--- Gerador de Curriculos/script.js
+++ Gerador de Curriculos/script.js
@@ -2,11 +2,11 @@ function salvar_informacoes(event) {
     event.preventDefault(); // Impede o envio do formulário
 
 
-    var nome = document.getElementById('name').value;
-    var idade = document.getElementById('idade').value;
-    var email = document.getElementById('email').value;
-    var telefone = document.getElementById('telefone').value;
-    var endereco = document.getElementById('endereco').value;
+    let nome = document.getElementById('name').value;
+    let idade = document.getElementById('idade').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let endereco = document.getElementById('endereco').value;
 
     localStorage.setItem('name', nome);
     localStorage.setItem('idade', idade);
@@ -20,17 +20,71 @@ function salvar_informacoes(event) {
 function salvar_informacoes2(event) {
     event.preventDefault();
 
-    var objetivo = document.getElementById('objetivo').value;
-    var experiencia = document.getElementById('experiencia').value;
-    var qualificacao_profissional = document.getElementById('qualificacao_profissional').value;
-    
+    let objetivo = document.getElementById('objetivo').value;
+
+
     localStorage.setItem('objetivo', objetivo);
-    localStorage.setItem('experiencia', experiencia);
-    localStorage.setItem('qualificacao_profissional', qualificacao_profissional);
 
     window.location.href = "pagina3.html"
 }
 
-function voltar(){
+function salvar_informacoes3(event) {
+    event.preventDefault();
+
+    let funcao = document.getElementById('funcao').value;
+    let empresa = document.getElementById('empresa').value;
+    let mes_inicio = document.getElementById('mes_inicio').value;
+    let ano_inicio = document.getElementById('ano_inicio').value;
+    let mes_termino = document.getElementById('mes_termino').value;
+    let ano_termino = document.getElementById('ano_termino').value;
+    let descricao = document.getElementById('descricao').value;
+
+    localStorage.setItem('funcao', funcao);
+    localStorage.setItem('empresa', empresa);
+    localStorage.setItem('mes_inicio', mes_inicio);
+    localStorage.setItem('ano_inicio', ano_inicio);
+    localStorage.setItem('mes_termino', mes_termino);
+    localStorage.setItem('ano_termino', ano_termino);
+    localStorage.setItem('descricao', descricao);
+
+    window.location.href = "pagina4.html"
+}
+
+// Tentando fazer a barrinha que retorna o nível da competência //
+
+const nivelOutput = document.getElementById('nivel-output_qualificacao');
+const descricao = document.getElementById('descricao');
+const nivelDescricao = document.getElementById('nivel-descricao');
+
+const niveis = {
+    1: 'Iniciante',
+    2: 'Intermediário',
+    3: 'Bom',
+    4: 'Muito bom',
+    5: 'Excelente',
+};
+
+document.getElementById('nivel_qualificacao').addEventListener('input', function () {
+    const nivel_qualificacao = this.value;
+    nivelOutput.textContent = niveis[nivel_qualificacao];
+    descricao.value = niveis[nivel_qualificacao]
+});
+
+// fim da tentativa //
+
+function salvar_informacoes4(event){
+    event.preventDefault();
+
+    let qualificacao_profissional = document.getElementById('qualificacao_profissional').value;
+    let nivel_qualificacao = document.getElementById('nivel_qualificacao').value;
+   
+   localStorage.setItem('qualificacao_profissional', qualificacao_profissional);
+   localStorage.setItem('nivel_qualificacao', nivel_qualificacao);
+
+   window.location.href = "pagina5.html"
+}
+
+
+function voltar() {
     window.history.back();
 }
