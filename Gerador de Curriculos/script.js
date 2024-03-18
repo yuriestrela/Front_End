2d4cff74922ee41b131235011af72fda076f0869
Yuri Estrella <141427497+yuriestrela@users.noreply.github.com>
2024-03-18 18:29:59 -0300

    fifth commit
    - Adicionado cursos na página de idioma;
    - Página 6 criada (layout do curriculo);
    - Mudança de let para const no script;
    - Tentativa de usar o valor do formulário falha

--- Gerador de Curriculos/script.js
+++ Gerador de Curriculos/script.js
@@ -1,12 +1,12 @@
 function salvar_informacoes(event) {
-    event.preventDefault(); // Impede o envio do formulário
+    event.preventDefault();
 
 
-    let nome = document.getElementById('name').value;
-    let idade = document.getElementById('idade').value;
-    let email = document.getElementById('email').value;
-    let telefone = document.getElementById('telefone').value;
-    let endereco = document.getElementById('endereco').value;
+    const nome = document.getElementById('name').value;
+    const idade = document.getElementById('idade').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const endereco = document.getElementById('endereco').value;
 
     localStorage.setItem('name', nome);
     localStorage.setItem('idade', idade);
@@ -20,7 +20,7 @@ function salvar_informacoes(event) {
 function salvar_informacoes2(event) {
     event.preventDefault();
 
-    let objetivo = document.getElementById('objetivo').value;
+    const objetivo = document.getElementById('objetivo').value;
 
 
     localStorage.setItem('objetivo', objetivo);
@@ -31,20 +31,20 @@ function salvar_informacoes2(event) {
 function salvar_informacoes3(event) {
     event.preventDefault();
 
-    let funcao = document.getElementById('funcao').value;
-    let empresa = document.getElementById('empresa').value;
-    let mes_inicio = document.getElementById('mes_inicio').value;
-    let ano_inicio = document.getElementById('ano_inicio').value;
-    let mes_termino = document.getElementById('mes_termino').value;
-    let ano_termino = document.getElementById('ano_termino').value;
-    let descricao = document.getElementById('descricao').value;
+    const funcao = document.getElementById('funcao').value;
+    const empresa = document.getElementById('empresa').value;
+    const mes_inicio_profissao = document.getElementById('mes_inicio_profissao').value;
+    const ano_inicio_profissao = document.getElementById('ano_inicio_profissao').value;
+    const mes_termino_profissao = document.getElementById('mes_termino_profissao').value;
+    const ano_termino_profissao = document.getElementById('ano_termino_profissao').value;
+    const descricao = document.getElementById('descricao').value;
 
     localStorage.setItem('funcao', funcao);
     localStorage.setItem('empresa', empresa);
-    localStorage.setItem('mes_inicio', mes_inicio);
-    localStorage.setItem('ano_inicio', ano_inicio);
-    localStorage.setItem('mes_termino', mes_termino);
-    localStorage.setItem('ano_termino', ano_termino);
+    localStorage.setItem('mes_inicio_profissao', mes_inicio_profissao);
+    localStorage.setItem('ano_inicio_profissao', ano_inicio_profissao);
+    localStorage.setItem('mes_termino_profissao', mes_termino_profissao);
+    localStorage.setItem('ano_termino_profissao', ano_termino_profissao);
     localStorage.setItem('descricao', descricao);
 
     window.location.href = "pagina4.html"
@@ -53,7 +53,7 @@ function salvar_informacoes3(event) {
 function salvar_informacoes4(event){
     event.preventDefault();
 
-    let competencia = document.getElementById('competencia').value;
+    const competencia = document.getElementById('competencia').value;
 
 
     localStorage.setItem('competencia', competencia);
@@ -61,38 +61,26 @@ function salvar_informacoes4(event){
     window.location.href = "pagina5.html"
 };
 
+
 function salvar_informacoes5(event){
     event.preventDefault();
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const nivelOutput = document.getElementById('nivel-output_idioma');
-        const nivelDescricao = document.getElementById('nivel-descricao');
-    
-        const niveis = {
-            0: 'Escolha uma opção',
-            1: 'Iniciante',
-            2: 'Básico',
-            3: 'Pré Intermediário',
-            4: 'Intermediário',
-            5: 'Avançado',
-            6: 'Fluente'
-        };
-    
-        document.getElementById('nivel_idioma').addEventListener('input', function () {
-            const nivel_idioma = this.value;
-            nivelOutput.textContent = niveis[nivel_idioma];
-            nivelDescricao.textContent = niveis[nivel_idioma]; 
-        });
-     
-        // Valor que inicia
-    
-        document.getElementById('nivel_qualificacao').value = 0;
-        nivelOutput.textContent = niveis[0];
-    });
+    const mes_inicio_curso = document.getElementById('mes_inicio_curso').value
+    const ano_inicio_curso = document.getElementById('ano_inicio_curso').value
+    const mes_termino_curso = document.getElementById('mes_termino_curso').value
+    const ano_termino_curso = document.getElementById('ano_termino_curso').value
+    const idioma = document.getElementById('idioma').value
 
-}
+    localStorage.setItem('mes_inicio_curso', mes_inicio_curso);
+    localStorage.setItem('ano_inicio_curso', ano_inicio_curso);
+    localStorage.setItem('mes_termino_curso', mes_termino_curso);
+    localStorage.setItem('ano_termino_curso', ano_termino_curso);
+    localStorage.setItem('idioma', idioma)
+
+    window.location.href = "pagina6.html"
+};
 
 function voltar() {
     window.history.back();
-}
+};
 
